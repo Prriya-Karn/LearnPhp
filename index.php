aa7c9a4c3b9c9630f0044f01a691326b5cb2dd55
Priya Karn <priya.karn.rn51@gmail.com>
2024-08-13 11:40:32 +0530

    start

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+// $name = "priya karn";
+// $age = 24;
+// $bool = true;
+// echo $name;
+// echo "<br>";
+// echo $age;
+
+// echo 20+"priya";    //error show
+
+// $num = 20;
+// if($num<=20){
+//     echo "true";
+// }else{
+//     echo "false";
+// }
+
+
+// $num = 10;
+// while($num>0){
+//     echo "priya <br>";
+//     $num--;
+// }
+
+
+// for($i=0;$i<=10;$i++){
+//     echo "priya ankita himmu <br>";
+// }
+
+
+
+// $arr = array("priya","ankita","himanshu",true);
+
+// echo $arr[3];
+
+//concatenation:-
+
+// $fName = "priya";
+// $lname = "karn";
+
+// echo 'My name is '.$fName.' my last name is' .$lname.'.';
+
+
+// function sum($a=10,$b=40){
+//     return $a+$b;
+// }
+
+// echo sum(20);
+
+// $names = array("priya","ankita","himanshu");
+
+// $arrayLength = count($names);
+// for($i=0;$i<$arrayLength;$i++){
+//     echo '<br>' .$names[$i]. '<br>';
+// }
+
+?>
+
